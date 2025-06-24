3e184403a3dc14020be53b333c16b20318580c0e
Mads Sørhaug <mgs@brreg.no>
2025-06-24 14:56:51 +0200

    Update fetch script

--- fetch-specs.sh
+++ fetch-specs.sh
@@ -1,10 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 echo "Generating all docs"
-../vaapen-kodelister-api/gradlew generateOpenApiDocs -p ../vaapen-kodelister-api
-../no.politiet.vaapen.blink.hendelse.api/gradlew generateOpenApiDocs -p ../no.politiet.vaapen.blink.hendelse.api
-../no.politiet.vaapen.personlig.api/gradlew generateOpenApiDocs -p ../no.politiet.vaapen.personlig.api
-../politiavgivelse-api/gradlew generateOpenApiDocs -p ../politiavgivelse-api
-../no.politiet.vaapen.virksomhet.api/gradlew generateOpenApiDocs -p ../no.politiet.vaapen.virksomhet.api
+
+my_dirs=("vaapen-kodelister-api" "no.politiet.vaapen.blink.hendelse.api" "no.politiet.vaapen.personlig.api" "politiavgivelse-api" "no.politiet.vaapen.virksomhet.api")
+
+for dir in "${my_dirs[@]}"; do
+  echo "Generating docs for $dir"
+  cd "../$dir" || exit
+
+  # Fetch latest from origin
+  git fetch origin master
+
+  # Get current branch name
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+  # Get local and remote commit hashes
+  local_commit=$(git rev-parse HEAD)
+  remote_commit=$(git rev-parse origin/master)
+
+  # Check if on master and in sync
+  if [ "$current_branch" = "master" ] && [ "$local_commit" = "$remote_commit" ]; then
+    echo "On master and in sync with origin/master. Proceeding..."
+    gradlew clean cleanDependencies generateOpenApiDocs
+  else
+    echo "Not on master or not in sync with origin/master. Aborting."
+    exit 1
+  fi
+done
+
+cd ../vaapen-openapi || exit
 
 echo "Fetching all openapi specs"
 cp ../vaapen-kodelister-api/build/docs/kodelister.yaml .
