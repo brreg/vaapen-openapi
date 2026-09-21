84b8f92e8046f56124494afee0c4a7c11ccc3bff
Espen Ønvik Pedersen <exteop@brreg.no>
2026-09-21 10:45:38 +0200

    Rett default-URL og fiks manglende output i update-swagger-workflow

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "",
+    url: new URLSearchParams(window.location.search).get('url') || "blink-hendelse-api.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -13,7 +13,9 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    defaultModelsExpandDepth: -1,
+    defaultModelExpandDepth: 1
   });
 
   //</editor-fold>
